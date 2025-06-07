ec42794786a9dedaae5d59999a8054f0f159c600
Jobinjohn2002 <jobin.john@excelenciaconsulting.com>
2025-06-07 21:31:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return "Hello from Flask on Azure VM!"
+    return "Hello from Flask on Azure VM! , This is for testing !!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
