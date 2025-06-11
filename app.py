b432851cc838eb7e0ce25b61e27b616b88bafe20
Jobinjohn2002 <jobin.john@excelenciaconsulting.com>
2025-06-11 16:57:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index():
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <title>User List</title>
+        <title>Users List</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
